e979353f42d901299413f27a5b153e8889f936b0
Anusha1109 <119651241+Anusha1109@users.noreply.github.com>
2023-08-07 15:16:03 +0530

    first_commit

--- bloodsbanks/wsgi.py
+++ bloodsbanks/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bbauet project.
+WSGI config for bloodsbanks project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bbauet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloodsbanks.settings')
 
 application = get_wsgi_application()
